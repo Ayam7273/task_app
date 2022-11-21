f968b9d950ff72a8be2129ed1c7a95542be29b67
ABDULBASIT ADIO <abdulbasitadio@gmail.com>
2022-11-21 15:09:42 +0100

    'env'

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-(*z6s)56ee3jyl=p34ul)j!mxmj=$5x_g-mb286rnigznr6wzo'
+SECRET_KEY = 'DJANGO_SECRET_KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
